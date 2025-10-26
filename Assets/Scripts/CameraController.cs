b3ac8b8d2ce7cf317ee2ec81f505b093989d8812
karripartanen <karripartanen25@gmail.com>
2025-10-26 17:29:28 +0200

    balls and walls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+ // Reference to the player GameObject.
+ public GameObject player;
+
+ private Vector3 offset;
+
+ void Start()
+    {
+        offset = transform.position - player.transform.position; 
+    }
+
+ void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;  
+    }
+}
