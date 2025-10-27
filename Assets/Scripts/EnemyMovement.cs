c44749612be93a4513521dab37314fd0bb9d1b1c
karripartanen <karripartanen25@gmail.com>
2025-10-27 18:00:03 +0200

    roll the damn ball

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -3,26 +3,19 @@ using UnityEngine.AI;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Reference to the player's transform.
-    public Transform player;
+ public Transform player;
 
-    // Reference to the NavMeshAgent component for pathfinding.
-    private NavMeshAgent navMeshAgent;
+ private NavMeshAgent navMeshAgent;
 
-    // Start is called before the first frame update.
-    void Start()
+ void Start()
     {
-        // Get and store the NavMeshAgent component attached to this object.
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
-    // Update is called once per frame.
-    void Update()
+ void Update()
     {
-        // If there's a reference to the player...
-        if (player != null)
-        {
-            // Set the enemy's destination to the player's current position.
+ if (player != null)
+        {    
             navMeshAgent.SetDestination(player.position);
         }
     }
